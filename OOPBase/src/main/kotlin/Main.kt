c4bc06655cd9de2a90d3a18fee0ddda96bed059f
Nikolai <nikolaiko@mail.ru>
2023-09-17 12:52:15 +0600

    If Tasks

---
+++ OOPBase/src/main/kotlin/Main.kt
@@ -0,0 +1,70 @@
+fun main(args: Array<String>) {
+
+}
+
+// Задачи по теме if-else
+fun ifFirstTask() {
+    var x = 6
+    var y = 8
+
+    if (y != 0) {
+        println(x / y)
+    } else {
+        println("Делить на ноль нельзя")
+    }
+
+    println("Программа закончена")
+}
+
+fun ifSecondTask() {
+    var p = 4
+    var b = 2
+    var fg = p + b
+    if (fg > 0){
+        println("Больше ноля")
+    } else {
+        println("Меньше ноля")
+    }
+}
+
+fun ifThirdTask() {
+    val month = 12
+    if (month == 12) {
+        println("December")
+    }
+    else if (month == 11) {
+        println("November")
+    }
+    else if (month == 10) {
+        println("October")
+    }
+    else if (month == 9) {
+        println("September")
+    }
+    else if (month == 8) {
+        println("August")
+    }
+    else if (month == 7) {
+        println("July")
+    }
+    else if (month == 6) {
+        println("June")
+    }
+    else if (month == 5) {
+        println("May")
+    }
+    else if (month == 4) {
+        println("April")
+    }
+    else if (month == 3) {
+        println("March")
+    }
+    else if (month == 2) {
+        println("February")
+    }
+    else if (month == 1) {
+        println("January")
+    } else {
+
+    }
+}
